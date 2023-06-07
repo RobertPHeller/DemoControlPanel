69e36e5e5d90c77e1e9e4c99ff0802503266e6ad
Robert Heller <heller@deepsoft.com>
2023-06-07 17:14:26 -0400

    Completed firmware.

---
+++ firmware/main.cxx
@@ -0,0 +1,312 @@
+// -!- C++ -!- //////////////////////////////////////////////////////////////
+//
+//  System        : 
+//  Module        : 
+//  Object Name   : $RCSfile$
+//  Revision      : $Revision$
+//  Date          : $Date$
+//  Author        : $Author$
+//  Created By    : Robert Heller
+//  Created       : Wed Jun 7 16:21:39 2023
+//  Last Modified : <230607.1711>
+//
+//  Description	
+//
+//  Notes
+//
+//  History
+//	
+/////////////////////////////////////////////////////////////////////////////
+//
+//    Copyright (C) 2023  Robert Heller D/B/A Deepwoods Software
+//			51 Locke Hill Road
+//			Wendell, MA 01379-9728
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// 
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include <ctype.h>
+#include "os/os.h"
+#include "nmranet_config.h"
+
+#include "openlcb/SimpleStack.hxx"
+#include "openlcb/ConfiguredConsumer.hxx"
+#include "openlcb/ConfiguredProducer.hxx"
+#include "openlcb/MultiConfiguredConsumer.hxx"
+#include "MultiConfiguredProducer.hxx"
+
+#include "config.hxx"
+#include "utils/GpioInitializer.hxx"
+#include "freertos_drivers/common/DummyGPIO.hxx"
+#include "freertos_drivers/common/LoggingGPIO.hxx"
+#include "os/LinuxGpio.hxx"
+#include "Hardware.hxx"
+
+// Changes the default behavior by adding a newline after each gridconnect
+// packet. Makes it easier for debugging the raw device.
+OVERRIDE_CONST(gc_generate_newlines, 1);
+// Specifies how much RAM (in bytes) we allocate to the stack of the main
+// thread. Useful tuning parameter in case the application runs out of memory.
+OVERRIDE_CONST(main_thread_stack_size, 2500);
+
+
+// easily incrementable method.
+#define DefaultNODEID 0x050101012200ULL // 05 01 01 01 22 00
+static openlcb::NodeID NODE_ID = DefaultNODEID;
+// ConfigDef comes from config.hxx and is specific to the particular device and
+// target. It defines the layout of the configuration memory space and is also
+// used to generate the cdi.xml file. Here we instantiate the configuration
+// layout. The argument of offset zero is ignored and will be removed later.
+openlcb::ConfigDef cfg(0);
+// Defines weak constants used by the stack to tell it which device contains
+// the volatile configuration information. This device name appears in
+// HwInit.cxx that creates the device drivers.
+
+char pathnamebuffer[256];
+
+extern const char *const openlcb::CONFIG_FILENAME = pathnamebuffer;
+// The size of the memory space to export over the above device.
+extern const size_t openlcb::CONFIG_FILE_SIZE =
+cfg.seg().size() + cfg.seg().offset();
+// The SNIP user-changeable information in also stored in the above eeprom
+// device. In general this could come from different eeprom segments, but it is
+// simpler to keep them together.
+extern const char *const openlcb::SNIP_DYNAMIC_FILENAME =
+openlcb::CONFIG_FILENAME;
+// Instantiates the actual producer and consumer objects for the given GPIO
+// pins from above. The ConfiguredConsumer class takes care of most of the
+// complicated setup and operation requirements. We need to give it the virtual
+// node pointer, the configuration configuration from the CDI definition, and
+// the hardware pin definition. The virtual node pointer comes from the stack
+// object. The configuration structure comes from the CDI definition object,
+// segment 'seg', in which there is a repeated group 'consumers', and we assign
+// the individual entries to the individual consumers. Each consumer gets its
+// own GPIO pin.
+
+
+class FactoryResetHelper : public DefaultConfigUpdateListener {
+public:
+    UpdateAction apply_configuration(int fd, bool initial_load,
+                                     BarrierNotifiable *done) OVERRIDE 
+    {
+        AutoNotify n(done);
+        return UPDATED;
+    }
+    void factory_reset(int fd) override
+    {
+        cfg.userinfo().name().write(fd, HARDWARE_IMPL);
+        cfg.userinfo().description().write(fd, HARDWARE_IMPL);
+    }
+};
+
+#ifdef USE_GRIDCONNECT_HOST
+int upstream_port = DEFAULT_TCP_GRIDCONNECT_PORT;
+const char *upstream_host = DEFAULT_TCP_GRIDCONNECT_HOST;
+#endif
+#ifdef USE_SOCKET_CAN_PORT
+const char *cansocket = DEFAULT_CAN_SOCKET;
+#endif
+
+// CLI Usage output.
+
+void usage(const char *e)
+{
+    fprintf(stderr, "Usage: %s [-e EEPROM_file_path] [-n NID] [-c LCC_CAN_SOCKET] [-p PNET_CAN_SOCKET]", e);
+    fprintf(stderr, "\n\n");    
+    fprintf(stderr, "Demo Control Panel.\n\n");
+    fprintf(stderr, "\nOptions:\n");
+    fprintf(stderr, "\t[-n nodeid]\n");
+    fprintf(stderr, "\t[-g host]\n");
+    fprintf(stderr, "\t[-c can_socketname]\n");
+    fprintf(stderr, "\t[-e EEPROM_file_path]\n");
+    fprintf(stderr, "Where:\n");
+    fprintf(stderr, "\tnodeid is the node id, as a 12 hex digit number (optionally with colons between pairs of hex digits.\n");
+    fprintf(stderr, "\thost the GC hub server to connect to\n");
+    fprintf(stderr, "\tcan_socketname the CAN socket for LCC\n");
+    fprintf(stderr, "\tEEPROM_file_path is the path to use to implement the EEProm device.\n");
+    exit(1);
+}
+
+// Parse CLI options.
+
+openlcb::NodeID parseNodeID(const char *nidstring)
+{
+    uint64_t result = 0ULL;
+    int nibcount = 0, coloncount = 0;
+    const char *p = NULL;
+    for (p = nidstring; *p != '\0'; p++)
+    {
+        if (isxdigit(*p))
+        {
+            nibcount++;
+            if (isdigit(*p))
+            {
+                result = (result<<4)+(*p-'0');
+            }
+            else if (islower(*p))
+            {
+                result = (result<<4)+(*p-'a'+10);
+            }
+            else
+            {
+                result = (result<<4)+(*p-'A'+10);
+            }
+        }
+        else if (*p == ':')
+        {
+            coloncount++;
+        }
+        else
+        {
+            // not a hex digit or colon
+            fprintf(stderr, "Syntax error: Illformed node id: %s\n",nidstring);
+            return (openlcb::NodeID) -1;
+        }
+    }
+    if (nibcount != 12)
+    {
+        // Wrong number of digits
+        fprintf(stderr, "Syntax error: Illformed node id: %s\n",nidstring);
+        return (openlcb::NodeID) -1;
+    }
+    if (coloncount != 0 && coloncount != 5)
+    {
+        // Wrong number of colons (some number other than 0 or 5)
+        fprintf(stderr, "Syntax error: Illformed node id: %s\n",nidstring);
+        return (openlcb::NodeID) -1;
+    }
+    return (openlcb::NodeID) result;
+}
+
+
+void parse_args(int argc, char *argv[])
+{
+    int opt;
+#define OPTSTRING "hn:e:p:g:h"
+    while ((opt = getopt(argc, argv, OPTSTRING)) >= 0)
+    {
+        switch (opt)
+        {
+        case 'h':
+            usage(argv[0]);
+            break;
+        case 'n':
+            {
+                openlcb::NodeID nid = parseNodeID(optarg);
+                if (((int64_t)nid) == -1) 
+                {
+                    usage(argv[0]);
+                }
+                else
+                {
+                    NODE_ID = nid;
+                }
+            }
+            break;
+        case 'e':
+            strncpy(pathnamebuffer,optarg,sizeof(pathnamebuffer));
+            break;
+#ifdef USE_SOCKET_CAN_PORT
+        case 'c':
+            cansocket = optarg;
+            break;
+#endif
+#ifdef USE_GRIDCONNECT_HOST
+        case 'g':
+            upstream_host = optarg;
+            break;
+#endif
+        default:
+            fprintf(stderr, "Unknown option %c\n", opt);
+            usage(argv[0]);
+        }
+    }
+}
+
+const Gpio *const kLEDs[] = {
+    LED1_Pin::instance(), LED2_Pin::instance(), LED3_Pin::instance(), 
+    LED4_Pin::instance(), LED5_Pin::instance(), LED6_Pin::instance(), 
+    LED7_Pin::instance(), LED8_Pin::instance(), LED9_Pin::instance(), 
+    LED10_Pin::instance(),LED11_Pin::instance(),LED12_Pin::instance(),
+    LED13_Pin::instance(),LED14_Pin::instance(),LED15_Pin::instance(),
+    LED16_Pin::instance(),LED17_Pin::instance(),LED18_Pin::instance(), 
+    LED19_Pin::instance(),LED20_Pin::instance(),LED21_Pin::instance()
+};
+
+const Gpio *const kButtons[] = {
+    S1_Pin::instance(),  S2_Pin::instance(),  S3_Pin::instance(), 
+    S4_Pin::instance(),  S5_Pin::instance(),  S6_Pin::instance(), 
+    S7_Pin::instance(),  S8_Pin::instance(),  S9_Pin::instance(),
+    S10_Pin::instance(), S11_Pin::instance(), S12_Pin::instance(), 
+    S13_Pin::instance(), S14_Pin::instance(), S15_Pin::instance(), 
+    S16_Pin::instance(), S17_Pin::instance(), S18_Pin::instance(), 
+    S19_Pin::instance(), S20_Pin::instance(), S21_Pin::instance()
+};
+
+/** Entry point to application.
+ * @param argc number of command line arguments
+ * @param argv array of command line arguments
+ * @return 0, should never return
+ */
+int appl_main(int argc, char *argv[])
+{
+    // Compute default EEProm and persistant train file pathnames.
+    snprintf(pathnamebuffer,sizeof(pathnamebuffer),
+             "/tmp/config_eeprom_%012llX",NODE_ID);
+    // Parse command line.
+    parse_args(argc, argv);
+    
+    // Initialize GPIO
+    GpioInit::hw_init();
+    
+    // Sets up a comprehensive OpenLCB stack for a single virtual node. This stack
+    // contains everything needed for a usual peripheral node -- all
+    // CAN-bus-specific components, a virtual node, PIP, SNIP, Memory configuration
+    // protocol, ACDI, CDI, a bunch of memory spaces, etc.
+    openlcb::SimpleCanStack stack(NODE_ID);
+    
+    openlcb::MultiConfiguredConsumer leds(stack.node(),
+                                          kLEDs,ARRAYSIZE(kLEDs),
+                                          cfg.seg().leds());
+    MultiConfiguredProducer buttons(stack.node(),
+                                    kButtons, ARRAYSIZE(kButtons),
+                                    cfg.seg().buttons());
+    openlcb::RefreshLoop buttonRefresh(stack.node(), 
+                                   {buttons.polling()});
+    FactoryResetHelper  factory_reset_helper;
+    // Create the config file
+    stack.create_config_file_if_needed(cfg.seg().internal_config(), openlcb::CANONICAL_VERSION, openlcb::CONFIG_FILE_SIZE);
+#ifdef USE_GRIDCONNECT_HOST
+    stack.connect_tcp_gridconnect_hub(upstream_host, upstream_port);
+#endif
+#ifdef PRINT_ALL_PACKETS
+    // Causes all packets to be dumped to stdout.
+    stack.print_all_packets();
+#endif
+#if defined(HAVE_SOCKET_CAN_PORT)
+    stack.add_socketcan_port_select(cansocket);
+#endif
+    
+    // This command donates the main thread to the operation of the
+    // stack. Alternatively the stack could be started in a separate stack and
+    // then application-specific business logic could be executed ion a busy
+    // loop in the main thread.
+    stack.loop_executor();
+    return 0;
+}
+
---
+++ firmware/targets/bbb.linux.armv7a/Hardware.hxx
@@ -0,0 +1,127 @@
+// -!- c++ -!- //////////////////////////////////////////////////////////////
+//
+//  System        : 
+//  Module        : 
+//  Object Name   : $RCSfile$
+//  Revision      : $Revision$
+//  Date          : $Date$
+//  Author        : $Author$
+//  Created By    : Robert Heller
+//  Created       : Wed Jun 7 13:57:51 2023
+//  Last Modified : <230607.1546>
+//
+//  Description	
+//
+//  Notes
+//
+//  History
+//	
+/////////////////////////////////////////////////////////////////////////////
+//
+//    Copyright (C) 2023  Robert Heller D/B/A Deepwoods Software
+//			51 Locke Hill Road
+//			Wendell, MA 01379-9728
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// 
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef __HARDWARE_HXX
+#define __HARDWARE_HXX
+
+#include <os/LinuxGpio.hxx>
+#include "utils/GpioInitializer.hxx"
+
+#define HARDWARE_IMPL "BBB Demo Control Panel"
+
+// On chip GPIO:
+//
+// LEDs
+#define LEDPin GpioOutputSafeLow
+                              // 64 Cable
+                              // P9.03 => 1 (+3.3V)
+                              // P9.04 => 2 (+3.3V)
+GPIO_PIN(LED1,  LEDPin, 30);  // P9.11 => 3
+GPIO_PIN(LED2,  LEDPin, 60);  // P9.12 => 4
+GPIO_PIN(LED3,  LEDPin, 31);  // P9.13 => 5
+GPIO_PIN(LED4,  LEDPin, 50);  // P9.14 => 6
+GPIO_PIN(LED5,  LEDPin, 48);  // P9.15 => 7
+GPIO_PIN(LED6,  LEDPin, 51);  // P9.16 => 8
+GPIO_PIN(LED7,  LEDPin, 49);  // P9.23 => 9
+GPIO_PIN(LED8,  LEDPin, 117); // P9.25 => 10
+GPIO_PIN(LED9,  LEDPin, 115); // P9.27 => 11
+GPIO_PIN(LED10, LEDPin, 113); // P9.28 => 12
+GPIO_PIN(LED11, LEDPin, 112); // P9.30 => 13
+GPIO_PIN(LED12, LEDPin, 110); // P9.31 => 14
+GPIO_PIN(LED13, LEDPin, 71);  // P8.46 => 15
+GPIO_PIN(LED14, LEDPin, 70);  // P8.45 => 16
+GPIO_PIN(LED15, LEDPin, 73);  // P8.44 => 17
+GPIO_PIN(LED16, LEDPin, 72);  // P8.43 => 18
+GPIO_PIN(LED17, LEDPin, 75);  // P8.42 => 19
+GPIO_PIN(LED18, LEDPin, 74);  // P8.41 => 20
+GPIO_PIN(LED19, LEDPin, 77);  // P8.40 => 21
+GPIO_PIN(LED20, LEDPin, 76);  // P8.39 => 22
+GPIO_PIN(LED21, LEDPin, 79);  // P8.38 => 23
+
+#define NUMLEDS 21
+
+//
+// Buttons
+#define ButtonPin GpioInputActiveLow
+GPIO_PIN(S15, ButtonPin, 78); // P8.37 => 42
+GPIO_PIN(S16, ButtonPin, 80); // P8.36 => 43
+GPIO_PIN(S17, ButtonPin, 8);  // P8.35 => 44
+GPIO_PIN(S18, ButtonPin, 81); // P8.34 => 45
+GPIO_PIN(S19, ButtonPin, 9);  // P8.33 => 46
+GPIO_PIN(S20, ButtonPin, 11); // P8.32 => 47
+GPIO_PIN(S21, ButtonPin, 10); // P8.31 => 48
+GPIO_PIN(S8,  ButtonPin, 89); // P8.30 => 49
+GPIO_PIN(S9,  ButtonPin, 87); // P8.29 => 50
+GPIO_PIN(S10, ButtonPin, 88); // P8.28 => 51
+GPIO_PIN(S11, ButtonPin, 86); // P8.27 => 52
+GPIO_PIN(S12, ButtonPin, 61); // P8.26 => 53
+GPIO_PIN(S13, ButtonPin, 22); // P8.19 => 54
+GPIO_PIN(S14, ButtonPin, 65); // P8.18 => 55
+GPIO_PIN(S1,  ButtonPin, 27); // P8.17 => 56
+GPIO_PIN(S2,  ButtonPin, 46); // P8.16 => 57
+GPIO_PIN(S3,  ButtonPin, 47); // P8.15 => 58
+GPIO_PIN(S4,  ButtonPin, 26); // P8.14 => 59
+GPIO_PIN(S5,  ButtonPin, 23); // P8.13 => 60
+GPIO_PIN(S6,  ButtonPin, 44); // P8.12 => 61
+GPIO_PIN(S7,  ButtonPin, 45); // P8.11 => 62
+                              // P8.02 => 63 (GND)
+                              // P8.01 => 64 (GND)
+
+#define NUMBUTTONS 21
+
+
+typedef GpioInitializer<LED1_Pin, LED2_Pin, LED3_Pin, LED4_Pin,
+LED5_Pin, LED6_Pin, LED7_Pin, LED8_Pin, LED9_Pin, LED10_Pin, LED11_Pin,
+LED12_Pin, LED13_Pin, LED14_Pin, LED15_Pin, LED16_Pin, LED17_Pin,
+LED18_Pin, LED19_Pin, LED20_Pin,LED21_Pin,
+S1_Pin, S2_Pin, S3_Pin, S4_Pin, S5_Pin, S6_Pin, S7_Pin, S8_Pin, S9_Pin,
+S10_Pin, S11_Pin, S12_Pin, S13_Pin, S14_Pin, S15_Pin, S16_Pin, S17_Pin,
+S18_Pin, S19_Pin, S20_Pin, S21_Pin> GpioInit;
+
+#define USE_SOCKET_CAN_PORT
+#define DEFAULT_CAN_SOCKET "can1"
+
+#define START_GCTCP_HUB
+#define DEFAULT_GRIDCONNECT_HUB_PORT 12021
+
+#endif // __HARDWARE_HXX
+
